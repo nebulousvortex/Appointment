ba87276d15c85b65184c3af04ca30ff62a2c10c5
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-01 06:04:54 +0300

    RegistrationController has been created to handle registration operation

---
+++ src/main/java/ru/sber/appointment/controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package ru.sber.appointment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.sber.appointment.model.User;
+import ru.sber.appointment.service.UserService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/registration")
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public String addUser(@RequestBody User user) {
+        if (!user.getPassword().equals(user.getPasswordConfirm())) {
+            return "Пароли не совпадают";
+        }
+        if (!userService.saveUser(user)) {
+            return "Пользователь с таким именем уже существует";
+        }
+        return "Пользователь успешно зарегистрирован";
+    }
+}
