1a4e5a5af11819444bbf9a7010e5802f44973b5a
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-01 05:45:31 +0300

    AuthController has been created to handle authentication purposes

---
+++ src/main/java/ru/sber/appointment/controller/AuthController.java
@@ -0,0 +1,45 @@
+package ru.sber.appointment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sber.appointment.model.jwt_model.JwtRequest;
+import ru.sber.appointment.model.jwt_model.JwtResponse;
+import ru.sber.appointment.model.RefreshJwtRequest;
+import ru.sber.appointment.service.AuthService;
+
+import javax.security.auth.message.AuthException;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthController {
+
+    @Autowired
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest authRequest) {
+        final JwtResponse token = authService.login(authRequest);
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("token")
+    public ResponseEntity<JwtResponse> getNewAccessToken(@RequestBody RefreshJwtRequest request) {
+        final JwtResponse token = authService.getAccessToken(request.getRefreshToken());
+        return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("refresh")
+    public ResponseEntity<JwtResponse> getNewRefreshToken(@RequestBody RefreshJwtRequest request) throws AuthException {
+        final JwtResponse token = authService.refresh(request.getRefreshToken());
+        return ResponseEntity.ok(token);
+    }
+
+}
