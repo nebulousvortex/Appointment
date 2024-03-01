7c4edbc5ff7b7616665d4111ff212cac54758493
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-01 06:14:57 +0300

    fixed using some non-existent methods

--- src/main/java/ru/sber/appointment/filter/DoctorFilter.java
+++ src/main/java/ru/sber/appointment/filter/DoctorFilter.java
@@ -11,13 +11,13 @@ public class DoctorFilter {
     public List<Doctor> filterDoctor(DoctorServiceImpl doctorService, Doctor doctor){
         List<Doctor> resultDoctorList = doctorService.findAllDoctors();
         System.out.println(resultDoctorList);
-        if (doctor.getFirstName() != null){
-            System.out.println(doctor.getFirstName());
-            resultDoctorList.retainAll(doctorService.findByFirstName(doctor.getFirstName()));
+        if (doctor.getUser().getFirstName() != null){
+            System.out.println(doctor.getUser().getFirstName());
+            resultDoctorList.retainAll(doctorService.findByFirstName(doctor.getUser().getFirstName()));
         }
-        if (doctor.getLastName() != null){
-            System.out.println(doctor.getLastName());
-            resultDoctorList.retainAll(doctorService.findByLastName(doctor.getLastName()));
+        if (doctor.getUser().getLastName() != null){
+            System.out.println(doctor.getUser().getLastName());
+            resultDoctorList.retainAll(doctorService.findByLastName(doctor.getUser().getLastName()));
         }
         if (doctor.getSpecialization() != null){
             System.out.println(doctor.getSpecialization());
