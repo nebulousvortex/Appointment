6a6bb1bdf544c78c153be097d45d76a3eccc1be1
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-01 05:41:24 +0300

    Removed fields that can be retrieved from the user model

--- src/main/java/ru/sber/appointment/model/Doctor.java
+++ src/main/java/ru/sber/appointment/model/Doctor.java
@@ -1,6 +1,9 @@
 package ru.sber.appointment.model;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
+import java.util.Set;
 
 
 @Entity
@@ -9,34 +12,28 @@ public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "Имя")
-    private String firstName;
-    @Column(name = "Фамилия")
-    private String lastName;
     @Column(name = "Должность")
     private String specialization;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public void setId(Long id) {
         this.id = id;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
     public void setSpecialization(String specialization) {
         this.specialization = specialization;
     }
-    public String getLastName() {
-        return lastName;
+    public void setUser(User user) {
+        this.user = user;
     }
     public String getSpecialization() {
         return specialization;
     }
-    public String getFirstName() {
-        return firstName;
-    }
     public Long getId() {
         return id;
+    }
+    public User getUser() {
+        return user;
     }
 }
