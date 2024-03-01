c464ee28e302d4e4d7f7b1d42faeda518421a06a
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-01 05:47:33 +0300

    AuthService has been created to handle authentication data

---
+++ src/main/java/ru/sber/appointment/service/AuthService.java
@@ -0,0 +1,82 @@
+package ru.sber.appointment.service;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.sber.appointment.configuration.JwtAuthentication;
+import ru.sber.appointment.model.jwt_model.JwtProvider;
+import ru.sber.appointment.model.jwt_model.JwtRequest;
+import ru.sber.appointment.model.jwt_model.JwtResponse;
+import ru.sber.appointment.model.User;
+
+import javax.security.auth.message.AuthException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private final UserService userService;
+    private final Map<String, String> refreshStorage = new HashMap<>();
+    @Autowired
+    private final JwtProvider jwtProvider;
+
+    public AuthService(UserService userService, JwtProvider jwtProvider) {
+        this.userService = userService;
+        this.jwtProvider = jwtProvider;
+    }
+
+    public JwtResponse login(@NonNull JwtRequest authRequest) {
+        final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        final User user = (User) userService.loadUserByUsername(authRequest.getLogin());
+        if ( bCryptPasswordEncoder.matches(authRequest.getPassword(), user.getPassword())){
+                //user.getPassword().equals(authRequest.getPassword())) {
+            final String accessToken = jwtProvider.generateAccessToken(user);
+            final String refreshToken = jwtProvider.generateRefreshToken(user);
+            refreshStorage.put(user.getUsername(), refreshToken);
+            return new JwtResponse(accessToken, refreshToken);
+        } else {
+            System.out.println("Неправильный пароль");
+        }
+        return null;
+    }
+
+    public JwtResponse getAccessToken(@NonNull String refreshToken) {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final String login = claims.getSubject();
+            final String saveRefreshToken = refreshStorage.get(login);
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final User user = (User) userService.loadUserByUsername(login);
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                return new JwtResponse(accessToken, null);
+            }
+        }
+        return new JwtResponse(null, null);
+    }
+
+    public JwtResponse refresh(@NonNull String refreshToken) throws AuthException {
+        if (jwtProvider.validateRefreshToken(refreshToken)) {
+            final Claims claims = jwtProvider.getRefreshClaims(refreshToken);
+            final String login = claims.getSubject();
+            final String saveRefreshToken = refreshStorage.get(login);
+            if (saveRefreshToken != null && saveRefreshToken.equals(refreshToken)) {
+                final User user = (User) userService.loadUserByUsername(login);
+                final String accessToken = jwtProvider.generateAccessToken(user);
+                final String newRefreshToken = jwtProvider.generateRefreshToken(user);
+                refreshStorage.put(user.getUsername(), newRefreshToken);
+                return new JwtResponse(accessToken, newRefreshToken);
+            }
+        }
+        throw new AuthException("Невалидный JWT токен");
+    }
+
+    public JwtAuthentication getAuthInfo() {
+        return (JwtAuthentication) SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
