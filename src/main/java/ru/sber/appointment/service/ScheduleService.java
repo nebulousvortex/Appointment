e8a41f9d47740723a2238826d97bca82ba0d41e3
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-04 09:44:56 +0300

    Added some CRUD operations for Schedule Model

--- src/main/java/ru/sber/appointment/service/ScheduleService.java
+++ src/main/java/ru/sber/appointment/service/ScheduleService.java
@@ -1,4 +1,52 @@
 package ru.sber.appointment.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.sber.appointment.model.DayType;
+import ru.sber.appointment.model.Doctor;
+import ru.sber.appointment.model.Schedule;
+import ru.sber.appointment.repository.DoctorRepository;
+import ru.sber.appointment.repository.ScheduleRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class ScheduleService {
+    @Autowired
+    DoctorRepository doctorRepository;
+    @Autowired
+    ScheduleRepository scheduleRepository;
+    @Autowired
+    TicketService ticketService;
+
+    public List<Schedule> findAllSchedule(){
+        return scheduleRepository.findAll();
+    }
+
+    public void saveSchedule(Schedule schedule){
+        scheduleRepository.save(schedule);
+        ticketService.saveForDay(schedule);
+    }
+
+    public void saveScheduleForWeek(Doctor unknownDoctor){
+        Doctor doctor = doctorRepository.findById(unknownDoctor.getId()).orElseThrow();
+        LocalDate date = LocalDate.now();
+        for (int i = 0; i < 7; i++) {
+            Schedule schedule = new Schedule();
+            schedule.setDoctor(doctor);
+            schedule.setDate(date);
+            schedule.setDayType(DayType.WORK);
+            scheduleRepository.save(schedule);
+            ticketService.saveForDay(schedule);
+            date = date.plusDays(1);
+        }
+    }
+
+    public void updateSchedule(Schedule unknownSchedule) {
+        Doctor doctor = doctorRepository.findById(unknownSchedule.getDoctor().getId()).orElseThrow();
+        Schedule schedule = scheduleRepository.findByDateAndDoctor(unknownSchedule.getDate(), doctor);
+        schedule.setDayType(unknownSchedule.getDayType());
+        scheduleRepository.save(schedule);
+    }
 }
