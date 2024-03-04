991911c3b434edaa027caecc327d8a6f0536e879
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-04 09:43:07 +0300

    Added method for finding Schedule by Date and Doctor

--- src/main/java/ru/sber/appointment/repository/ScheduleRepository.java
+++ src/main/java/ru/sber/appointment/repository/ScheduleRepository.java
@@ -1,7 +1,11 @@
 package ru.sber.appointment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.sber.appointment.model.Doctor;
 import ru.sber.appointment.model.Schedule;
+import java.time.LocalDate;
+
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    Schedule findByDateAndDoctor(LocalDate date, Doctor doctor);
 }
