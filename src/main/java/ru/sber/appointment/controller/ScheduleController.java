a61f817a5c296ec014e581298a3763b74e8f25bd
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-04 09:46:28 +0300

    Added ScheduleController for pages related with admin schedules

---
+++ src/main/java/ru/sber/appointment/controller/ScheduleController.java
@@ -0,0 +1,38 @@
+package ru.sber.appointment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.sber.appointment.model.Doctor;
+import ru.sber.appointment.model.Schedule;
+import ru.sber.appointment.service.ScheduleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/schedule")
+public class ScheduleController {
+
+    @Autowired
+    ScheduleService scheduleService;
+
+    @GetMapping("/get")
+    public List<Schedule> getSchedule(){
+        return scheduleService.findAllSchedule();
+    }
+
+    @PostMapping("/post")
+    public void saveSchedule(@RequestBody(required = false) Schedule schedule){
+        scheduleService.saveSchedule(schedule);
+    }
+
+    @PostMapping("/postForWeek")
+    public void saveScheduleForWeek(@RequestBody Doctor doctor){
+        scheduleService.saveScheduleForWeek(doctor);
+    }
+
+    @PutMapping("/updateSchedule")
+    public void updateDayType(@RequestBody Schedule schedule){
+        scheduleService.updateSchedule(schedule);
+    }
+}
+
