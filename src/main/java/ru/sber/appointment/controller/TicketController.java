d4559b13db30cff4fff134991c1e616297a6e232
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-09 03:09:01 +0300

    Added TicketController for Ticket related queries

---
+++ src/main/java/ru/sber/appointment/controller/TicketController.java
@@ -0,0 +1,31 @@
+package ru.sber.appointment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.sber.appointment.model.Ticket;
+import ru.sber.appointment.service.ScheduleService;
+import ru.sber.appointment.service.TicketService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/ticket")
+public class TicketController {
+    @Autowired
+    TicketService ticketService;
+
+    @GetMapping("/getAll")
+    public List<Ticket> getTickets(){
+        return ticketService.findAllTickets();
+    }
+
+    @PostMapping("/post")
+    public void saveTicket(@RequestBody(required = false) Ticket ticket){
+        ticketService.saveTicket(ticket);
+    }
+
+    @PutMapping("/appointUser")
+    public void updateTicket(@RequestBody Ticket ticket){
+        ticketService.updateTicket(ticket);
+    }
+}
