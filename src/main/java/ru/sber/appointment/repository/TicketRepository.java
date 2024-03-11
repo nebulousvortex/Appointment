d2ca19c3a44e38303d5d218d60ab8ecf74fbdb9f
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-12 00:01:02 +0300

    Added a method for returning a list of tickets by schedule.

--- src/main/java/ru/sber/appointment/repository/TicketRepository.java
+++ src/main/java/ru/sber/appointment/repository/TicketRepository.java
@@ -1,7 +1,13 @@
 package ru.sber.appointment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.sber.appointment.model.Schedule;
 import ru.sber.appointment.model.Ticket;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findAllByScheduleIn(List<Schedule> schedules);
+
+    List<Ticket> findAllBySchedule(Schedule schedule);
 }
