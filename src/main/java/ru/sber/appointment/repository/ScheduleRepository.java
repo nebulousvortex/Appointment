7c613d02ba10d79f323547d6777c80a104f73d3d
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-12 00:02:06 +0300

    Added a method for returning a list of schedules by doctor and time before.

--- src/main/java/ru/sber/appointment/repository/ScheduleRepository.java
+++ src/main/java/ru/sber/appointment/repository/ScheduleRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sber.appointment.model.Doctor;
 import ru.sber.appointment.model.Schedule;
 import java.time.LocalDate;
+import java.util.List;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Schedule findByDateAndDoctor(LocalDate date, Doctor doctor);
+    List<Schedule> findAllByDoctor(Doctor doctor);
+
+    List<Schedule> findAllByDoctorAndDateAfter(Doctor doctor, LocalDate now);
 }
