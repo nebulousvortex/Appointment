964ca94380f756b95083f3cdd7d489005d5df053
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-13 14:28:51 +0300

    Now the data is validated during registration

--- src/main/java/ru/sber/appointment/controller/RegistrationController.java
+++ src/main/java/ru/sber/appointment/controller/RegistrationController.java
@@ -1,26 +1,41 @@
 package ru.sber.appointment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.sber.appointment.model.User;
 import ru.sber.appointment.service.UserService;
-import org.springframework.http.ResponseEntity;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/registration")
 public class RegistrationController {
 
     @Autowired
-    private UserService userService;
+    UserService userService;
     @PostMapping
-    public ResponseEntity addUser(@RequestBody User user) {
-        if (!user.getPassword().equals(user.getPasswordConfirm())) {
-            return ResponseEntity.ok("Пароли не совпадают");
+    public ResponseEntity<?> addUser(@Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorMessages = bindingResult.getFieldErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(errorMessages);
+        } else {
+            if (!user.getPassword().equals(user.getPasswordConfirm())) {
+                return ResponseEntity.ok("Пароли не совпадают");
+            }
+            if (!userService.saveUser(user)) {
+                return ResponseEntity.ok("Пользователь с таким именем уже существует");
+            }
+            return ResponseEntity.ok(null);
         }
-        if (!userService.saveUser(user)) {
-            return ResponseEntity.ok("Пользователь с таким именем уже существует");
-        }
-        return ResponseEntity.ok("Пользователь успешно зарегистрирован");
     }
 }
