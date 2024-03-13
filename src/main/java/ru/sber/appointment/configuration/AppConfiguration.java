d3993a326e1faddad703a785d710729edaa732ed
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-13 14:16:54 +0300

    Added configuration for asynchronous execution.

---
+++ src/main/java/ru/sber/appointment/configuration/AppConfiguration.java
@@ -0,0 +1,24 @@
+package ru.sber.appointment.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AppConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("MyAsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
