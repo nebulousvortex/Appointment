298dac03e29e793c0d22288fdef04f568870d502
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-13 14:30:58 +0300

    Added user search

--- src/main/java/ru/sber/appointment/repository/TicketRepository.java
+++ src/main/java/ru/sber/appointment/repository/TicketRepository.java
@@ -3,11 +3,12 @@ package ru.sber.appointment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sber.appointment.model.Schedule;
 import ru.sber.appointment.model.Ticket;
+import ru.sber.appointment.model.User;
 
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findAllByScheduleIn(List<Schedule> schedules);
-
     List<Ticket> findAllBySchedule(Schedule schedule);
+    List<Ticket> findAllByUser(User user);
 }
