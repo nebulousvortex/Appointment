895277d944358292ce6bbf001cad6a88de4f2759
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-13 14:24:44 +0300

    The approach to the response in authorization has been changed. Now it stores information about the user in addition to the token

--- src/main/java/ru/sber/appointment/jwt_manager/JwtResponse.java
+++ src/main/java/ru/sber/appointment/jwt_manager/JwtResponse.java
@@ -1,18 +1,35 @@
 package ru.sber.appointment.jwt_manager;
 
+import ru.sber.appointment.model.User;
+
 public class JwtResponse {
 
     private final String type = "Bearer";
     private String accessToken;
     private String refreshToken;
+    private User user;
 
     public JwtResponse(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
     }
 
+    public JwtResponse(String accessToken, String refreshToken, User user) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.user = user;
+    }
+
     public String getType() {
         return type;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
     }
 
     public String getAccessToken() {
