614736cfb9a0205ce44b2206fc29432ec78c8212
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-18 13:14:14 +0300

    During initialization, users are added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,34 @@ INSERT INTO t_role (name) VALUES ('ROLE_USER');
 INSERT INTO t_role (name) VALUES ('ROLE_DOCTOR');
 INSERT INTO t_role (name) VALUES ('ROLE_ADMIN');
 
+
+
+-- Админ: Пароль admin
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Слобожанин', 'Федор', 'Владимирович', 'vortexofnebula@mail.ru', '$2a$10$jRWvBoWkYZ2Oxq6VF6gv.uLctq5EhhuTrUnz9IanYWD/lc/xDNhRy', '89009696053', '0000 0000 0000 0000');
+
+-- Доктор Петр Петров Петрович: Пароль doctor
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Петров', 'Петр', 'Петрович', 'mega_pixel_pro@mail.ru', '$2a$10$7U7tNQFCj7XnsbBfZiFhReUEhkKB2G/4k/wZdoANBMGVmQatFRx6q', '89009696053', '0000 0000 0000 0001');
+
+-- Доктор Иван Иванович Иванов: Пароль doctor
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Иванов', 'Иван', 'Иванович', 'mega_pixel_pro@mail.ru', '$2a$10$7U7tNQFCj7XnsbBfZiFhReUEhkKB2G/4k/wZdoANBMGVmQatFRx6q', '89009696053', '0000 0000 0000 0002');
+
+-- Доктор Федор Федорович Федоров: Пароль doctor
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Федоров', 'Федор', 'Федорович', 'mega_pixel_pro@mail.ru', '$2a$10$7U7tNQFCj7XnsbBfZiFhReUEhkKB2G/4k/wZdoANBMGVmQatFRx6q', '89009696053', '0000 0000 0000 0003');
+
+-- Доктор Александр Иванович Петров: Пароль doctor
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Петров', 'Александр', 'Иванович', 'mega_pixel_pro@mail.ru', '$2a$10$7U7tNQFCj7XnsbBfZiFhReUEhkKB2G/4k/wZdoANBMGVmQatFRx6q', '89009696053', '0000 0000 0000 0004');
+
+-- Пользователь Петр Федорович Иванов: Пароль user
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Иванов', 'Петр', 'Федорович', 'mega_pixel_pro@mail.ru', '$2a$10$MDmp5S6Tl3DjWoLtM1wMkuGau3B7sAoaNkmqTy/yu7UZzRWKcPIVC', '89009696053', '0000 0000 0000 0005');
+
+-- Пользователь Федор Александрович Федоров: Пароль user
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Федоров', 'Федор', 'Александрович', 'mega_pixel_pro@mail.ru', '$2a$10$MDmp5S6Tl3DjWoLtM1wMkuGau3B7sAoaNkmqTy/yu7UZzRWKcPIVC', '89009696053', '0000 0000 0000 0006');
+
+-- Пользователь Федор Петрович Александров: Пароль user
+INSERT INTO t_user (date_of_birth, last_name, first_name, sur_name, mail, password, phone, username) VALUES ('2001-06-21 00:00:00', 'Александров', 'Федор', 'Петрович', 'mega_pixel_pro@mail.ru', '$2a$10$MDmp5S6Tl3DjWoLtM1wMkuGau3B7sAoaNkmqTy/yu7UZzRWKcPIVC', '89009696053', '0000 0000 0000 0007');
+
+
+
+INSERT INTO t_user_roles(user_id, role_id) VALUES (1, 3), (2, 2), (3, 2), (4, 2), (5, 2), (6, 1), (7, 1), (8, 1);
+
+INSERT INTO doctors(specialization, user_id) VALUES ('Окулист', 2), ('Терапевт', 3), ('Хирург', 4), ('Терапевт', 5);
