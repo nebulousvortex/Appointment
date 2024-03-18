ba8b130dd58a5499937fad30a1a5c885dfda13c2
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-18 13:09:56 +0300

    Added a bean for initializing the Telegram bot

--- src/main/java/ru/sber/appointment/configuration/AppConfiguration.java
+++ src/main/java/ru/sber/appointment/configuration/AppConfiguration.java
@@ -1,9 +1,13 @@
 package ru.sber.appointment.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.concurrent.Executor;
 
@@ -21,4 +25,10 @@ public class AppConfiguration implements AsyncConfigurer {
         executor.initialize();
         return executor;
     }
+
+    @Bean
+    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+
 }
