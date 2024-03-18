14ae117c64ca71e832f43d0756b7ba204324fb2f
NebulousVortex <VortexOfNebula@mail.ru>
2024-03-18 14:05:27 +0300

    Methods are extracted from the conditions. Validation has been added.

--- src/main/java/ru/sber/appointment/service/BotService.java
+++ src/main/java/ru/sber/appointment/service/BotService.java
@@ -10,17 +10,16 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.sber.appointment.model.Doctor;
 import ru.sber.appointment.model.Ticket;
+import ru.sber.appointment.model.User;
 
 import java.util.List;
 
 @Service
 public class BotService extends TelegramLongPollingBot {
-    private TelegramBotsApi telegramBotsApi;
 
     @Autowired
     public BotService(TelegramBotsApi telegramBotsApi) throws TelegramApiException {
-        this.telegramBotsApi = telegramBotsApi;
-        this.telegramBotsApi.registerBot(this);
+        telegramBotsApi.registerBot(this);
     }
 
     @Autowired
@@ -32,57 +31,25 @@ public class BotService extends TelegramLongPollingBot {
     @Autowired
     EmailServiceImpl emailService;
     private String username;
-    private String specialization;
     private List<Doctor> doctors;
     private List<Ticket> tikets;
     @Override
     public void onUpdateReceived(Update update) {
         String chatId = update.getMessage().getChatId().toString();
-        String text = update.getMessage().getText() + update.getMessage().getFrom();
         SendMessage sendMessage = new SendMessage();
 
         if (update.getMessage().isCommand()){
-            if(update.getMessage().getText().startsWith("/login ")){
-                username = update.getMessage().getText().substring(7);
-                sendMessage.setChatId(chatId);
-                sendMessage.setText("Привет, " + username);
+            if(update.getMessage().getText().startsWith("/login")){
+                login(update, sendMessage, chatId);
             }
-            if(update.getMessage().getText().startsWith("/doctors ")){
-                specialization = update.getMessage().getText().substring(9);
-                sendMessage.setChatId(chatId);
-                doctors = doctorService.findBySpecialization(specialization);
-                StringBuilder textBuilder = new StringBuilder();
-                for (int i = 0; i < doctors.size(); i++) {
-                    Doctor doctor = doctors.get(i);
-                    String doctorInfo = (i + 1) + ". " + doctor.getUser().getFirstName() + " " + doctor.getUser().getLastName() + ", " + doctor.getSpecialization();
-                    textBuilder.append(doctorInfo).append("\n");
-                }
-                sendMessage.setText(textBuilder.toString());
+            if(update.getMessage().getText().startsWith("/doctors")){
+                getDoctors(update, sendMessage, chatId);
             }
-            if(update.getMessage().getText().startsWith("/ticket ")){
-                int doctorNumber = Integer.parseInt(update.getMessage().getText().substring(8));
-                sendMessage.setChatId(chatId);
-                tikets = ticketService.findDoctorFreeTicket(doctors.get(doctorNumber - 1));
-                StringBuilder textBuilder = new StringBuilder();
-                int size = 0;
-                if (tikets.size()>20){
-                    size = 20;
-                } else {
-                    size = tikets.size();
-                }
-                for (int i = 0; i < size; i++) {
-                    Ticket ticket = tikets.get(i);
-                    String ticketInfo = (i + 1) + ". Дата: " + ticket.getSchedule().getDate() + " Время:  " + ticket.getTime();
-                    textBuilder.append(ticketInfo).append("\n");
-                }
-                sendMessage.setText(textBuilder.toString());
+            if(update.getMessage().getText().startsWith("/ticket")){
+                getTickets(update, sendMessage, chatId);
             }
-            if(update.getMessage().getText().startsWith("/appoint ")){
-                int doctorNumber = Integer.parseInt(update.getMessage().getText().substring(9));
-                sendMessage.setChatId(chatId);
-                Ticket ticket = tikets.get(doctorNumber-1);
-                emailService.sendEmailWithQR(userService.findByUsername(username).getMail(), username, ticket.getId());
-                sendMessage.setText("Вам на почту выслан QR код. Просканируйте в течение 10 минут, чтобы подтвердить запись на прием.");
+            if(update.getMessage().getText().startsWith("/appoint")){
+                sendQR(update, sendMessage, chatId);
             }
         }
 
@@ -91,6 +58,74 @@ public class BotService extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void sendQR(Update update, SendMessage sendMessage, String chatId) {
+        if(validate(sendMessage, chatId)) {
+            int doctorNumber = Integer.parseInt(update.getMessage().getText().substring(9));
+            sendMessage.setChatId(chatId);
+            Ticket ticket = tikets.get(doctorNumber - 1);
+            emailService.sendEmailWithQR(userService.findByUsername(username).getMail(), username, ticket.getId());
+            sendMessage.setText("Вам на почту выслан QR код. Просканируйте в течение 10 минут, чтобы подтвердить запись на прием.");
+        }
+    }
+
+    private void getTickets(Update update, SendMessage sendMessage, String chatId) {
+        int doctorNumber = Integer.parseInt(update.getMessage().getText().substring(8));
+        sendMessage.setChatId(chatId);
+        tikets = ticketService.findDoctorFreeTicket(doctors.get(doctorNumber - 1));
+        StringBuilder textBuilder = new StringBuilder();
+        int size = 0;
+        if (tikets.size()>20){
+            size = 20;
+        } else {
+            size = tikets.size();
+        }
+        for (int i = 0; i < size; i++) {
+            Ticket ticket = tikets.get(i);
+            String ticketInfo = (i + 1) + ". Дата: " + ticket.getSchedule().getDate() + " Время:  " + ticket.getTime();
+            textBuilder.append(ticketInfo).append("\n");
+        }
+        sendMessage.setText(textBuilder.toString());
+    }
+
+    private void getDoctors(Update update, SendMessage sendMessage, String chatId) {
+        if (update.getMessage().getText().length() > 9 ) {
+            String specialization = update.getMessage().getText().substring(9);
+            doctors = doctorService.findBySpecialization(specialization);
+        }
+        sendMessage.setChatId(chatId);
+        if(doctors == null || doctors.isEmpty()){
+            doctors = doctorService.findAllDoctors();
+        }
+        StringBuilder textBuilder = new StringBuilder();
+        for (int i = 0; i < doctors.size(); i++) {
+            Doctor doctor = doctors.get(i);
+            String doctorInfo = (i + 1) + ". " + doctor.getUser().getFirstName() + " " + doctor.getUser().getLastName() + ", " + doctor.getSpecialization();
+            textBuilder.append(doctorInfo).append("\n");
+        }
+        sendMessage.setText(textBuilder.toString());
+    }
+
+    private void login(Update update, SendMessage sendMessage, String chatId) {
+        username = update.getMessage().getText().substring(7);
+        sendMessage.setChatId(chatId);
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            sendMessage.setText("Пользователь не найден!");
+            username = null;
+        } else {
+            sendMessage.setText("Здравствуйте, " + user.getFirstName() + ' ' + user.getLastName() + '!');
+        }
+    }
+
+    private boolean validate(SendMessage sendMessage, String chatId){
+        sendMessage.setChatId(chatId);
+        if (username == null | doctors == null | tikets == null){
+            sendMessage.setText("Вы заполнили не все данные для записи");
+            return false;
+        }
+        return true;
     }
 
     @Override
